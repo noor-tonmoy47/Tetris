b4d7e5f2af7f67bb440dc4934a2ca9884d936244
noor-tonmoy47 <noortonmoy47@gmail.com>
2022-08-13 22:21:03 +0600

    Created Tiles and Player class and defined their properties

---
+++ src/object/Player.java
@@ -0,0 +1,39 @@
+package object;
+
+import java.io.Serializable;
+
+public class Player implements Serializable {
+    private  String name;
+
+    private int score;
+
+
+    public Player(String name, int score){
+        this.name = name;
+        this.score = score;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getScore() {
+        return score;
+    }
+
+    public void setScore(int score) {
+        this.score = score;
+    }
+
+    @Override
+    public String toString(){
+        return "Player{" +
+                "name='" + name + '\'' +
+                ", score=" + score +
+                '}';
+    }
+}
